ef64fc870ad57d21423d5c7b69d9fa2c4235598e
yuhaokun139 <georgehk139@gmail.com>
2026-05-09 15:25:18 +0800

    Update labassignment.py

--- labassignment.py
+++ labassignment.py
@@ -22,8 +22,8 @@ def text2story(text):
     start = f"In this picture, we can see {text}."
     
     prompt = (
-        f"Continue the story based on the picture: {text} "
         f"Write a complete, happy, and simple children's story (50 to 100 words) for a kid aged 3-10.\n"
+        f"The story based on the picture: {text} "
         f"Story: {start}"
         )
     
