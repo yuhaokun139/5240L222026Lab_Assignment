83e4545d1539046292bc9705e39c01095659a6f2
yuhaokun139 <georgehk139@gmail.com>
2026-05-09 13:57:12 +0800

    Update labassignment.py

--- labassignment.py
+++ labassignment.py
@@ -32,7 +32,7 @@ def text2story(text):
     story_results = story_pipe(
         prompt, 
         max_new_tokens = 150,
-        min_nex_tokens = 80,
+        min_new_tokens = 80,
         do_sample = True,
         temperature = 0.4,
         repetition_penalty = 1.2,
